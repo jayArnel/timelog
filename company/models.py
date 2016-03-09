e752137064929e11ede138713a46573bce0a7ad0
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-09 10:03:40 +0800

    created Company model

--- company/models.py
+++ company/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Company(models.Model):
+
+    name = models.CharField(max_length=40)
+    admin = models.OneToOneField(User, related_name='company')
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
