68a450ea5153431585f2f33b5a7f8ab8b88d5b41
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-17 16:41:49 +0800

    updated js to add a log via ajax

--- log/static/log/js/timelog.js
+++ log/static/log/js/timelog.js
@@ -1,5 +1,28 @@
-require(['jquery'], function($){
+require(['jquery', 'cookies'], function($, cookies){
     var container = $('.container#timelog');
+
+    /**
+     * Add a log instance for the user
+     * @param  {[Event]} e [description]
+     * @return none
+     */
+    function timein(e){
+        e.preventDefault();
+        var pk = container.data('user');
+        $.ajax({
+            url: $(this).attr('href'),
+            type: 'post',
+            data: {'pk': pk},
+            beforeSend: function(xhr, settings) {
+                var csrftoken = cookies.get('csrftoken');
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(response) {
+                console.log('timed in');
+            },
+        })
+    }
+
     /**
      * Bind actions in timelog page
      * @return none
@@ -8,11 +31,4 @@ require(['jquery'], function($){
         container.find('#timein').on('click', timein);
     })();
 
-    /**
-     * Add a log instance for the user
-     * @return {[type]} [description]
-     */
-    function timein(){
-        
-    }
 });
