5cf0c6305f3c77de2dd3b193f099643b83570739
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-17 13:18:17 +0800

    created post function for the login form

--- log/views.py
+++ log/views.py
@@ -1,7 +1,31 @@
-from django.views.generic import TemplateView, View
+import json
 
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.views.generic import TemplateView, View
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'log/home/home.html'
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            msg = {}
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    msg['status'] = 401
+                    msg['redirect_url'] = '/log'
+                else:
+                    msg['error'] = "This user account is deactivated. \
+                        Please contact your administrator"
+                    msg['status'] = 401
+            else:
+                msg['error'] = "Invalid username/password combination"
+                msg['status'] = 401
+            return HttpResponse(json.dumps(msg))
+        return HttpResponseForbidden()
