d429dcfba03c0523a29ca0fd3e7e44e84dce40cb
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-17 19:27:34 +0800

    updated js to handle time out

--- log/static/log/js/timelog.js
+++ log/static/log/js/timelog.js
@@ -23,12 +23,30 @@ require(['jquery', 'cookies'], function($, cookies){
         })
     }
 
+    function timeout(e){
+        e.preventDefault();
+        var pk = container.data('user');
+        $.ajax({
+            url: $(this).attr('href'),
+            type: 'post',
+            data: {'pk': pk},
+            beforeSend: function(xhr, settings) {
+                var csrftoken = cookies.get('csrftoken');
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(response) {
+                console.log('timed out');
+            },
+        })
+    }
+
     /**
      * Bind actions in timelog page
      * @return none
      */
     (function bindActions() {
         container.find('#timein').on('click', timein);
+        container.find('#timeout').on('click', timeout);
     })();
 
 });
