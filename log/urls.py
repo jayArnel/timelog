0c7c1b8602db7ad61a794bfcc71f704670c001ce
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-17 15:24:26 +0800

    added url for the log out view

--- log/urls.py
+++ log/urls.py
@@ -5,4 +5,5 @@ import views
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^log/$', views.LogView.as_view(), name='log'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
 ]
