14e05649bfa46cbd0cc5b9aa9cd036e2c0bf1166
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-22 17:30:25 +0800

    updated urls after using only one view for time logging

--- log/urls.py
+++ log/urls.py
@@ -5,7 +5,6 @@ import views
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^log/$', views.LogView.as_view(), name='log'),
-    url(r'^timein/$', views.TimeInView.as_view(), name='timein'),
-    url(r'^timeout/$', views.TimeOutView.as_view(), name='timeout'),
+    url(r'^time/$', views.TimeView.as_view(), name='timein'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
 ]
