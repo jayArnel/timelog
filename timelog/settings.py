c97804f0afd3059798936a1b41d641f88fc19b47
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-03-22 11:37:04 +0800

    added csrf and static template context processors

--- timelog/settings.py
+++ timelog/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.static',
+                'django.template.context_processors.csrf',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
